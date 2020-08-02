2a9327b59336095488e56c0977b082d878507fd1
ifnir <13046122+Ifnir@users.noreply.github.com>
2020-08-02 16:07:23 +0200

    first init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,130 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+    <img class="parent" src="../assets/resources/bg_30FRM_loop.gif" />
+    
+      <img v-if="expression === undefined || expression === null" class="child" src="../assets/resources/idle/MAIN_FRAME.gif" />
+
+      <img v-if="expression === 'idle1'" class="child" src="../assets/resources/idle/IDLE_15_FRMS_V2.gif" />
+      <img v-if="expression === 'idle2'" class="child" src="../assets/resources/idle/IDLE_25_FRMS_V3.gif" />
+      <img v-if="expression === 'idle3'" class="child" src="../assets/resources/idle/IDLE_15_FRMS_V1.gif" />
+   
   </div>
 </template>
-
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data: () => ({
+    recognition: null,
+    talking: false,
+    expression: null,
+    list: ['undefined', 'idle1', 'idle2', 'idle3']
+  }),
+  methods: {
+    timer() {
+      setInterval(() => {
+        // make switch for transition expression from normal to angry or kind
+        // make if statement to determine what list to use when transitioned
+          this.expression = this.list[this.random()];
+          setTimeout(() => {
+              this.expression = null;
+          }, 5000)
+      }, 12000)
+    },
+    random() {
+      return Math.floor(Math.random() * Math.floor(this.list.length)) + 1;
+    },
+    face(face) {
+      switch(face) {
+        case 'angry':
+        break;
+        case 'kind':
+        break;
+        case 'normal':
+        break;
+      }
+    },
+    startup() {
+      const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+      if (!SpeechRecognition && "development" !== 'production') {
+        throw new Error('Speech Recognition does not exist on this browser. Use Chrome or Firefox');
+      }
+      if (!SpeechRecognition) {
+        console.log("No Speech Recognition");
+        return;
+      }
+      console.log("Starting UP");
+      this.recognition = SpeechRecognition? new SpeechRecognition() : false;
+
+      this.recognition.lang = 'en-US'
+      this.recognition.continuous = true
+      this.recognition.interimResults = false;
+
+    },
+    stopSpeech(){
+      this.recognition.stop();
+    },
+    startSpeech(){
+      try{ 
+        this.recognition.start();
+    }
+      catch(e){
+        console.log(e)
+      }
+    },
+    detectSilence(
+      stream,
+    onSoundEnd = {},
+    onSoundStart = {},
+    silence_delay = 500,
+    min_decibels = -80) {
+      const ctx = new AudioContext();
+      const analyser = ctx.createAnalyser();
+      const streamNode = ctx.createMediaStreamSource(stream);
+      streamNode.connect(analyser);
+      analyser.minDecibels = min_decibels;
+
+      const data = new Uint8Array(analyser.frequencyBinCount); // will hold our data
+      let silence_start = performance.now();
+      let triggered = false; // trigger only once per silence event
+
+      function loop(time) {
+        requestAnimationFrame(loop); // we'll loop every 60th of a second to check
+        analyser.getByteFrequencyData(data); // get current data
+        if (data.some(v => v)) { // if there is data above the given db limit
+          if(triggered){
+            triggered = false;
+            onSoundStart();
+            }
+          silence_start = time; // set it to now
+        }
+        if (!triggered && time - silence_start > silence_delay) {
+          onSoundEnd();
+          triggered = true;
+        }
+      }
+      loop();
+    }
+  },
+  created() {
+    navigator.mediaDevices.getUserMedia({audio:true}).then(stream => this.detectSilence(stream, this.stopSpeech, this.startSpeech))
+    .catch(e => console.log(e.message));
+  },
+  mounted() {
+    this.startup();
+    this.timer();
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .parent {
+    position: relative;
+    top: 0;
+    left: 0;
+  }
+  .child {
+    position: absolute;
+    top: 9x;
+    left: 0;
+  }
 </style>
+
